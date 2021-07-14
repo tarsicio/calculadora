e9693cd68b864d923ca3dc5cf3609c3c4a462716
Tarsicio Carrizales <telecom.com.ve@gmail.com>
2021-07-13 23:51:02 -0400

    Create restar.blade.php

---
+++ horus/tarsicio/calculadora/src/views/restar.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calculadora</title>
+</head>
+<body>
+	<h1 style="text-align:center">
+		El Resultado de la Resta es:  
+		<span style="font-weight:normal">{{ $result }}</span>
+	</h1>
+</body>
+</html>
