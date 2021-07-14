534976d90bd8394cf28034c69620140ed6940908
Tarsicio Carrizales <telecom.com.ve@gmail.com>
2021-07-14 15:30:14 -0400

    Add files via upload

---
+++ horus/tarsicio/calculadora/src/database/migrations/2021_05_18_161653_create_tiendas_visitadas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * @author Tarsicio Carrizales
+ */
+class CreateTiendasVisitadasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tiendas_visitadas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('stores_id')->nullable()->index();
+            $table->bigInteger('users_id')->nullable()->index();
+            $table->string('contexto')->nullable();
+            $table->boolean('logueado')->default(false);
+            $table->text('properties')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tiendas_visitadas');
+    }
+}
